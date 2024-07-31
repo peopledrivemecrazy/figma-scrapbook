3f74bbda81dbf141d49af233735e8987d89b3e9b
shriji <shrijike@gmail.com>
2024-07-30 22:27:11 -0400

    add refresh logic

--- src/lib/server/arctic.ts
+++ src/lib/server/arctic.ts
@@ -1,4 +1,23 @@
 import { CLIEND_ID, CLIENT_SECRET, APP_URL } from '$env/static/private';
-import { Figma } from 'arctic';
+import { Figma, OAuth2RequestError } from 'arctic';
 
 export const figma = new Figma(CLIEND_ID, CLIENT_SECRET, `${APP_URL}/auth`);
+
+export const refreshSession = async (refreshToken: string) => {
+	try {
+		const tokens = await figma.refreshAccessToken(refreshToken);
+		const access_token = tokens.accessToken();
+		const refresh_token = tokens.refreshToken();
+		const expires_in = tokens.accessTokenExpiresInSeconds();
+
+		return {
+			access_token,
+			refresh_token,
+			expires_in
+		};
+	} catch (e) {
+		if (e instanceof OAuth2RequestError) {
+			console.log(e);
+		}
+	}
+};
