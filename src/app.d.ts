07cd995fa33704e37c0ab851bf38753569655e21
shriji <shrijike@gmail.com>
2024-07-30 22:27:47 -0400

    add base User type

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,15 @@
 // See https://kit.svelte.dev/docs/types#app
+
+import type { User } from "$lib";
+
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			access_token: string;
+			user: User;
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
