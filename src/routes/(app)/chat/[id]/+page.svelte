aabe6c88132b107167c19e426aeeea7bbb835b28
shriji <shrijike@gmail.com>
2024-08-04 18:58:10 -0400

    add chat bubble

--- src/routes/(app)/chat/[id]/+page.svelte
+++ src/routes/(app)/chat/[id]/+page.svelte
@@ -4,4 +4,24 @@
 	export let data: PageData;
 </script>
 
-<code><pre>{JSON.stringify(data.thread, null, 4)}</pre></code>
+<div class="p-4">
+	{#if data.thread.length}
+		<ul>
+			{#each data.thread as { message, user }}
+				<li class="flex justify-between bg-yellow-400/90 p-2">
+					<p>
+						{message}
+					</p>
+					<div class="flex items-center gap-4">
+						<img src={user.img_url} alt="{user.handle}'s avatar" class="w-8 rounded-full" />
+						<p>
+							{user.handle}
+						</p>
+					</div>
+				</li>
+			{/each}
+		</ul>
+	{:else}
+		<p>No messages</p>
+	{/if}
+</div>
